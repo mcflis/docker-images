42924569ad9b70b96caf6a54c49cf3fba0f38be8
Maximilian Flis <maximilian.flis@gmail.com>
2022-06-25 15:36:09 +0200

    update indentation

--- restic/entry.sh
+++ restic/entry.sh
@@ -6,8 +6,8 @@
 echo "Starting container ..."
 
 if [ -n "${NFS_TARGET}" ]; then
-    echo "Mounting NFS based on NFS_TARGET: ${NFS_TARGET}"
-    sh -c "mount -o nolock -v ${NFS_TARGET} /mnt/restic"
+  echo "Mounting NFS based on NFS_TARGET: ${NFS_TARGET}"
+  sh -c "mount -o nolock -v ${NFS_TARGET} /mnt/restic"
 fi
 
 sh -c "restic snapshots ${RESTIC_INIT_ARGS} > /dev/null 2>&1"
@@ -15,22 +15,20 @@ status=$?
 echo "Check Repo status $status"
 
 if [ $status != 0 ]; then
-    echo "Restic repository '${RESTIC_REPOSITORY}' does not exists. Running restic init."
-    sh -c "restic init ${RESTIC_INIT_ARGS}"
+  echo "Restic repository '${RESTIC_REPOSITORY}' does not exists. Running restic init."
+  sh -c "restic init ${RESTIC_INIT_ARGS}"
 
-    init_status=$?
-    echo "Repo init status $init_status"
+  init_status=$?
+  echo "Repo init status $init_status"
 
-    if [ "$init_status" -ne 0 ]; then
-        echo "Failed to init the repository: '${RESTIC_REPOSITORY}'"
-        exit 1
-    fi
+  if [ "$init_status" -ne 0 ]; then
+    echo "Failed to init the repository: '${RESTIC_REPOSITORY}'"
+    exit 1
+  fi
 fi
 
-
-
 echo "Setup backup cron job with cron expression BACKUP_CRON: ${BACKUP_CRON}"
-echo "${BACKUP_CRON} /usr/bin/flock -n /var/run/backup.lock /bin/backup >> /var/log/cron.log 2>&1" > /var/spool/cron/crontabs/root
+echo "${BACKUP_CRON} /usr/bin/flock -n /var/run/backup.lock /bin/backup >> /var/log/cron.log 2>&1" >/var/spool/cron/crontabs/root
 
 # Make sure the file exists before we start tail
 touch /var/log/cron.log
